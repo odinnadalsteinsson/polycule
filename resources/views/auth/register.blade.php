cde28bfc13653140637002aa3e7b8a7a52b33d4b
Odinn Adalsteinsson <oad@dwarf.dk>
2017-11-22 19:33:15 +0100

    Added images from polydating.dk

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <v-layout column>
   <v-flex xs12 sm6 offset-sm3>
     <v-card hover>
-      <v-card-media class="white--text" height="300px" src="https://unsplash.it/400/300?random">
+      <v-card-media class="white--text" height="300px" src="{{ asset('images/love-images-1-1500x938.jpg') }}">
         <v-container fill-height fluid>
           <v-layout fill-height>
             <v-flex xs12 align-end flexbox>
